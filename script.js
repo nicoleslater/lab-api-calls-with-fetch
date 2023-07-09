8c6386dcc74a3cb6201cbc61b2420aea39ecf351
Nicole Slater <nicoleslater@pursuit.org>
2023-07-09 12:47:56 -0400

    Fetching

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 
-console.log(button)
+
 
 const url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean"
 
 const button = document.querySelector('button[type="submit"]')
 
+const centeredSection = document.querySelector(".centered")
+
+
+
+button.addEventListener("click", function (){
+    console.log("firing click")
+    console.log(data)
+    
+    fetch(url)
+
+    .then(data => data.json())
+    
+
+    .catch(err => alert("Try Again"));
+})
+
+const showQuestion = (json) => {
+
+    centeredSection.innerHTML =  `
+        <article class="card">
+        <h2>CATEGORY</h2>
+        <p>QUESTION</p>
+        <button>Show Answer</button>
+        <p class="hidden">CORRECT ANSWER</p>
+        </article>
+    
+    `
+}
