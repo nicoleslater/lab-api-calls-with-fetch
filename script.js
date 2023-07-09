34dafab5e05ee1923dffad47bb35938b6f1900c5
Nicole Slater <nicoleslater@pursuit.org>
2023-07-09 12:18:33 -0400

    Working through Script.JS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log(button)
+
+const url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean"
+
+const button = document.querySelector('button[type="submit"]')
+
